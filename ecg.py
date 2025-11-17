97dd194667bb125bc1db3c03be5907cf1254e69f
GemaRicoMarchenaUMA <gemarico2@uma.es>
2025-11-17 12:29:48 +0100

    Añadido contenido a archivos

---
+++ ecg.py
@@ -0,0 +1,9 @@
+# Generate 15 seconds of ECG signal (recorded at 250 samples/second)
+ecg = nk.ecg_simulate(duration=15, sampling_rate=250, heart_rate=70)
+
+# Process it
+signals, info = nk.ecg_process(ecg, sampling_rate=250)
+
+# Visualise the processing
+nk.ecg_plot(signals, info)
+print("Hola mundo")
